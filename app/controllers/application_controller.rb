85fd564cde572d453c5f298c82bf83e0ad0141eb
William Jones <williammasonjones@gmail.com>
2015-03-12 11:18:01 -0400

    added force_json method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
  protect_from_forgery with: :null_session
+ before_action :force_json!
 
   def authenticate_user_from_token!
     user_token = request.headers[:HTTP_AUTH_TOKEN]
@@ -18,6 +19,11 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound do
     render json: nil, status: :not_found
+  end
+
+  # Allows use of :create when rendering json instead of using file path of view
+  def force_json!
+    request.format = :json
   end
 
 end
