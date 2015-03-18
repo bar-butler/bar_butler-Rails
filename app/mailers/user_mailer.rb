931ea6427a2bf6452443feb4fffeb6c69faf37d1
Andrew Mercer <diddydodge27@yahoo.com>
2015-03-18 12:31:13 -0400

    mailer message

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,10 @@ class UserMailer < ActionMailer::Base
     mail(
       to: email,
       subject: "You are almost out of beer!"
+      body: "This message is to inform you that your current keg of '' is running low.
+            Please refill as soon as possible.
+                                    -Sincerely,
+                                     Bar Butler"
     )
   end
 end
